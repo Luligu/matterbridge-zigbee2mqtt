a943c8973b7ab0f36e635eb167364e5831e804dd
Luligu <132135057+Luligu@users.noreply.github.com>
2024-07-11 11:22:54 +0200

    Release 2.1.7

---
+++ src/index.test.ts
@@ -0,0 +1,33 @@
+import { Matterbridge, PlatformConfig } from 'matterbridge';
+import { AnsiLogger } from 'matterbridge/logger';
+import { ZigbeePlatform } from './platform.js';
+import initializePlugin from './index';
+import { jest } from '@jest/globals';
+
+describe('initializePlugin', () => {
+  let mockMatterbridge: Matterbridge;
+  let mockLog: AnsiLogger;
+  let mockConfig: PlatformConfig;
+
+  beforeEach(() => {
+    mockMatterbridge = { addBridgedDevice: jest.fn(), matterbridgeDirectory: '', matterbridgePluginDirectory: '' } as unknown as Matterbridge;
+    mockLog = { error: jest.fn(), warn: jest.fn(), info: jest.fn(), debug: jest.fn() } as unknown as AnsiLogger;
+    mockConfig = {
+      'name': 'matterbridge-test',
+      'type': 'DynamicPlatform',
+      'noDevices': false,
+      'throwLoad': false,
+      'throwStart': false,
+      'throwConfigure': false,
+      'throwShutdown': false,
+      'unregisterOnShutdown': false,
+      'delayStart': false,
+    } as PlatformConfig;
+  });
+
+  it('should return an instance of TestPlatform', () => {
+    const result = initializePlugin(mockMatterbridge, mockLog, mockConfig);
+
+    expect(result).toBeInstanceOf(ZigbeePlatform);
+  });
+});
