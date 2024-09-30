1ac73f136c4e44478dc89e23e5b4106e8701bc5c
Luligu <132135057+Luligu@users.noreply.github.com>
2024-09-30 08:58:39 +0200

    Dev 2.1.18-dev.1

--- link-matterbridge-script.js
+++ link-matterbridge-script.js
@@ -1,15 +1,1 @@
-/* eslint-disable no-console */
-import { exec } from 'child_process';
-
-const command = process.platform === 'win32' ? 'npm link matterbridge' : 'sudo npm link matterbridge';
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-  }
-});
+// Nothing to do in production, just a script to link the matterbridge package in development
